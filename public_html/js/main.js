85514510e9eef757a06fa3410dd55f6a1f23e1a3
Ojutiku Oluwatobi <xtobsmagic@aol.com>
2016-03-04 17:04:51 +0400

    Include a sample way to use TextBox notify functionality

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-    $('#textarea-one').textBox();
+    $('#textarea-one').textBox({
+        onEdit: function() {
+            $('#toggle_btn').text("Preview");
+        },
+        onPreview: function() {
+            $('#toggle_btn').text("Edit");
+        }
+    });
+
     $('#textarea-two').textBox();
     $('#textarea-three').textBox();
 
@@ -8,4 +16,16 @@ $(document).ready(function() {
     $('#nbs-textarea-three').textBox();
 
     $('[data-toggle]').popover();
+
+
+    $('#toggle_btn').click(function() {
+        var textBox = $('#textarea-one').data('textbox');
+
+        if (textBox.inPreview()) {
+            textBox.edit();
+        } else {
+            textBox.preview();
+        }
+    });
+
 });
